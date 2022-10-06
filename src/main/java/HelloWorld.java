e8161c1e805b6166ef938ddaa6549122ac1ee168
GitForProjects <mametjob@gmail.com>
2022-10-06 14:38:09 -0400

    Commit 3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world 1!");
-        System.out.println("Hello world 2!");
-        System.out.println("Hello world 3!");
+        System.out.println("I am ...");
+        System.out.println("I do ...");
+        System.out.println("I have ....");
     }
 }
